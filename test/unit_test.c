4e193f0e7f466bf169bbfa9a8c5d166e9164bedb
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-10-04 12:12:05 +0200

    Update unit_test.c

--- test/unit_test.c
+++ test/unit_test.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+#include "vector.h"
+
+int main() {
+    /// ########################### INIT ###########################
+    Vector v1;
+    init(&v1);
+    assert(&v1 != NULL);
+    assert(v1.data != NULL);
+    assert(v1.capacity == 1);
+    assert(v1.length == 0);
+
+    Vector *v2 = NULL;
+    v2 = init(v2);
+    assert(v2 != NULL);
+    assert(v2->data != NULL);
+    assert(v2->capacity == 1);
+    assert(v2->length == 0);
+
+
+    Vector *v3 = malloc(sizeof(Vector));
+    init(v3);
+    assert(v3 != NULL);
+    assert(v3->data != NULL);
+    assert(v3->capacity == 1);
+    assert(v3->length == 0);
+
+    /// ######################## PUSH_FLOAT ########################
+
+    /// ######################### PUSH_ARR #########################
+
+    /// ########################### POP ############################
+
+    /// ######################### SET_SIZE #########################
+
+    /// ########################## SHRINK ##########################
+
+    /// ########################## VECCPY ##########################
+
+
+    return 0;
+}
